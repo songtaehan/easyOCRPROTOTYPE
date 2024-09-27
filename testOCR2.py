b52fc77150dd3ab8b2d064d7259e4323905f3b73
songtaehan <102862593+songtaehan@users.noreply.github.com>
2024-09-27 09:28:26 +0900

    Update testOCR2.py

--- testOCR2.py
+++ testOCR2.py
@@ -8,7 +8,7 @@ print(new_result)
 split_result = [i.split() for i in result]
 print(split_result)
 go = list()
-info = list()
+info1 = list()
 info2 = list()
 info3 = list()
 info4 = list()
@@ -16,18 +16,16 @@ info5 = list()
 info6 = list()
 info7 = list()
 info8 = list()
-info9 = list()
-info10 = list()
-info = [go for go in split_result if 'kcal' in go ]
+
+info1 = [go for go in split_result if 'kcal' in go ]
 info2 = [go for go in split_result if '나트륨' in go ]
 info3 = [go for go in split_result if '탄수화물' in go ]
 info4 = [go for go in split_result if '당류' in go ]
 info5 = [go for go in split_result if '지방' in go ]
-info6 = [go for go in split_result if '트랜스지방' in go ]
-info7 = [go for go in split_result if '포화지방' in go ]
-info8 = [go for go in split_result if '콜레스테롤' in go ]
-info9 = [go for go in split_result if '단백질' in go ]
-print(info)
+info6 = [go for go in split_result if '포화지방' in go ]
+info7 = [go for go in split_result if '콜레스테롤' in go ]
+info8 = [go for go in split_result if '단백질' in go ]
+print(info1)
 print(info2)
 print(info3)
 print(info4)
@@ -35,11 +33,40 @@ print(info5)
 print(info6)
 print(info7)
 print(info8)
-print(info9)
+
 
 totalinfo = list()
-totalinfo = info + info2 + info3 + info4 + info5 + info6 + info7 + info8 + info9
+totalinfo = info1 + info2 + info3 + info4 + info5 + info6 + info7 + info8  
 print(totalinfo)
 
 
-totalinfo[1][1]
+print(totalinfo[0][0])
+print(totalinfo[1][1])
+print(totalinfo[2][1])
+print(totalinfo[3][1])
+print(totalinfo[4][1])
+print(totalinfo[5][1])
+
+
+data0 = totalinfo[0][0]
+data1 = totalinfo[1][1]
+data2 = totalinfo[2][1]
+data3 = totalinfo[3][1]
+data4 = totalinfo[4][1]
+data5 = totalinfo[5][1]
+
+
+data1 = data1[:-1]
+data2 = data2[:-1]
+data3 = data3[:-1]
+data4 = data4[:-1]
+data5 = data5[:-1]
+
+print("----------------------")
+print("데이터 추출 완료")
+print(data0) #kcal
+print(data1) #탄수화물
+print(data2) #당류
+print(data3) #지방
+print(data4) #포화지방
+print(data5) #단백질
